e29014f4992fa65defe89cdb94ee6a5fccf419db
bsoniam <soniam.bogos@gmail.com>
2018-05-17 16:55:27 +0200

    Added first version of access control tests for WSFED

--- tests/business_tests/test_CT_TC_SAML_IDP_ACCESS_CONTROL_RBAC_KO.py
+++ tests/business_tests/test_CT_TC_SAML_IDP_ACCESS_CONTROL_RBAC_KO.py
@@ -181,133 +181,132 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_RBAC_KO():
 
         assert re.search(sp2_message, response.text) is not None
 
-
     def test_CT_TC_SAML_IDP_ACCESS_CONTROL_RBAC_KO_IDP_initiated(self, settings):
-            """
-            Scenario: User logs in to the IDP. He then accesses SP1 where he has the appropriate role.
-            Same user tries to log in to SP2, that he is not authorized to access. He should receive an
+        """
+        Scenario: User logs in to the IDP. He then accesses SP1 where he has the appropriate role.
+        Same user tries to log in to SP2, that he is not authorized to access. He should receive an
         error message saying he has not the authorization to access SP2.
-            :param settings:
-            :return:
-            """
+        :param settings:
+        :return:
+        """
 
-            s = Session()
+        s = Session()
 
-            # Service provider settings
-            sp = settings["sps_saml"][0]
-            sp_ip = sp["ip"]
-            sp_port = sp["port"]
-            sp_scheme = sp["http_scheme"]
-            sp_path = sp["path"]
-            sp_message = sp["logged_in_message"]
+        # Service provider settings
+        sp = settings["sps_saml"][0]
+        sp_ip = sp["ip"]
+        sp_port = sp["port"]
+        sp_scheme = sp["http_scheme"]
+        sp_path = sp["path"]
+        sp_message = sp["logged_in_message"]
 
-            # Service provider 2 settings
-            sp2 = settings["sps_saml"][2]
-            sp2_ip = sp2["ip"]
-            sp2_port = sp2["port"]
-            sp2_scheme = sp2["http_scheme"]
-            sp2_path = sp2["path"]
-            sp2_message = settings["idp"]["not_authorized_message"]
+        # Service provider 2 settings
+        sp2 = settings["sps_saml"][2]
+        sp2_ip = sp2["ip"]
+        sp2_port = sp2["port"]
+        sp2_scheme = sp2["http_scheme"]
+        sp2_path = sp2["path"]
+        sp2_message = settings["idp"]["not_authorized_message"]
 
-            # Identity provider settings
-            idp_ip = settings["idp"]["ip"]
-            idp_port = settings["idp"]["port"]
-            idp_scheme = settings["idp"]["http_scheme"]
-            idp_message = settings["idp"]["logged_in_message"]
-            idp_test_realm = settings["idp"]["test_realm"]["name"]
-            idp_path = "auth/realms/{realm}/account".format(realm=idp_test_realm)
+        # Identity provider settings
+        idp_ip = settings["idp"]["ip"]
+        idp_port = settings["idp"]["port"]
+        idp_scheme = settings["idp"]["http_scheme"]
+        idp_message = settings["idp"]["logged_in_message"]
+        idp_test_realm = settings["idp"]["test_realm"]["name"]
+        idp_path = "auth/realms/{realm}/account".format(realm=idp_test_realm)
 
-            idp_username = settings["idp"]["test_realm"]["username"]
-            idp_password = settings["idp"]["test_realm"]["password"]
+        idp_username = settings["idp"]["test_realm"]["username"]
+        idp_password = settings["idp"]["test_realm"]["password"]
 
-            # Common header for all the requests
-            header = {
-                'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                'Accept-Encoding': "gzip, deflate",
-                'Accept-Language': "en-US,en;q=0.5",
-                'User-Agent': "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0",
-                'Connection': "keep-alive",
-                'Upgrade-Insecure-Requests': "1",
-            }
+        # Common header for all the requests
+        header = {
+            'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            'Accept-Encoding': "gzip, deflate",
+            'Accept-Language': "en-US,en;q=0.5",
+            'User-Agent': "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0",
+            'Connection': "keep-alive",
+            'Upgrade-Insecure-Requests': "1",
+        }
 
-            # Perform login to IDP
-            (oath_cookie, keycloak_cookie, keycloak_cookie2, response) = req.login_idp(logger, s, header, idp_ip,
-                                                                                       idp_port, idp_scheme,
-                                                                                       idp_path, idp_username,
-                                                                                       idp_password)
+        # Perform login to IDP
+        (oath_cookie, keycloak_cookie, keycloak_cookie2, response) = req.login_idp(logger, s, header, idp_ip,
+                                                                                   idp_port, idp_scheme,
+                                                                                   idp_path, idp_username,
+                                                                                   idp_password)
 
-            assert response.status_code == HTTPStatus.OK
+        assert response.status_code == HTTPStatus.OK
 
-            # Assert we are logged in to IDP
-            assert re.search(idp_message, response.text) is not None
+        # Assert we are logged in to IDP
+        assert re.search(idp_message, response.text) is not None
 
-            # Access SP1
-            (session_cookie, response) = req.access_sp_saml(logger, s, header, sp_ip, sp_port, sp_scheme, sp_path,
-                                                            idp_ip, idp_port)
+        # Access SP1
+        (session_cookie, response) = req.access_sp_saml(logger, s, header, sp_ip, sp_port, sp_scheme, sp_path,
+                                                        idp_ip, idp_port)
 
-            # store the cookie received from keycloak
-            keycloak_cookie3 = response.cookies
+        # store the cookie received from keycloak
+        keycloak_cookie3 = response.cookies
 
-            assert response.status_code == HTTPStatus.FOUND
+        assert response.status_code == HTTPStatus.FOUND
 
-            redirect_url = response.headers['Location']
+        redirect_url = response.headers['Location']
 
-            header_redirect_idp = {
-                **header,
-                'Host': "{ip}:{port}".format(ip=idp_ip, port=idp_port),
-                'Referer': "{ip}:{port}".format(ip=sp_ip, port=sp_port)
-            }
+        header_redirect_idp = {
+            **header,
+            'Host': "{ip}:{port}".format(ip=idp_ip, port=idp_port),
+            'Referer': "{ip}:{port}".format(ip=sp_ip, port=sp_port)
+        }
 
-            response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp,
-                                           {**keycloak_cookie3, **keycloak_cookie2})
+        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp,
+                                       {**keycloak_cookie3, **keycloak_cookie2})
 
-            assert response.status_code == HTTPStatus.OK
+        assert response.status_code == HTTPStatus.OK
 
-            soup = BeautifulSoup(response.content, 'html.parser')
-            form = soup.body.form
+        soup = BeautifulSoup(response.content, 'html.parser')
+        form = soup.body.form
 
-            url_form = form.get('action')
-            inputs = form.find_all('input')
-            method_form = form.get('method')
+        url_form = form.get('action')
+        inputs = form.find_all('input')
+        method_form = form.get('method')
 
-            # Get the saml response from the identity provider
-            saml_response = {}
-            for input in inputs:
-                saml_response[input.get('name')] = input.get('value')
+        # Get the saml response from the identity provider
+        saml_response = {}
+        for input in inputs:
+            saml_response[input.get('name')] = input.get('value')
 
-            (response, sp_cookie) = req.access_sp_with_token(logger, s, header, sp_ip, sp_port, idp_scheme, idp_ip,
-                                                             idp_port,
-                                                             method_form, url_form, saml_response, session_cookie,
-                                                             keycloak_cookie2)
+        (response, sp_cookie) = req.access_sp_with_token(logger, s, header, sp_ip, sp_port, idp_scheme, idp_ip,
+                                                         idp_port,
+                                                         method_form, url_form, saml_response, session_cookie,
+                                                         keycloak_cookie2)
 
-            assert response.status_code == HTTPStatus.OK
+        assert response.status_code == HTTPStatus.OK
 
-            assert re.search(sp_message, response.text) is not None
+        assert re.search(sp_message, response.text) is not None
 
-            # User can access SP1
+        # User can access SP1
 
-            # Attempt to access SP2
+        # Attempt to access SP2
 
-            (session_cookie, response) = req.access_sp_saml(logger, s, header, sp2_ip, sp2_port, sp2_scheme, sp2_path,
-                                                            idp_ip,
-                                                            idp_port)
+        (session_cookie, response) = req.access_sp_saml(logger, s, header, sp2_ip, sp2_port, sp2_scheme, sp2_path,
+                                                        idp_ip,
+                                                        idp_port)
 
-            session_cookie2 = response.cookies
+        session_cookie2 = response.cookies
 
-            redirect_url = response.headers['Location']
+        redirect_url = response.headers['Location']
 
-            header_redirect_idp = {
-                **header,
-                'Host': "{ip}:{port}".format(ip=idp_ip, port=idp_port),
-                'Referer': "{ip}:{port}".format(ip=sp2_ip, port=sp2_port)
-            }
+        header_redirect_idp = {
+            **header,
+            'Host': "{ip}:{port}".format(ip=idp_ip, port=idp_port),
+            'Referer': "{ip}:{port}".format(ip=sp2_ip, port=sp2_port)
+        }
 
-            response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp,
-                                           {**session_cookie2, **keycloak_cookie2})
+        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp,
+                                       {**session_cookie2, **keycloak_cookie2})
 
-            # Assert that the client is not authorized to access SP2
-            assert response.status_code == HTTPStatus.FORBIDDEN
+        # Assert that the client is not authorized to access SP2
+        assert response.status_code == HTTPStatus.FORBIDDEN
 
-            assert re.search(sp2_message, response.text) is not None
+        assert re.search(sp2_message, response.text) is not None
 
 
--- tests/business_tests/test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_ABAC_OK.py
+++ tests/business_tests/test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_ABAC_OK.py
@@ -7,6 +7,7 @@
 import pytest
 import logging
 import re
+import json
 
 import helpers.requests as req
 from http import HTTPStatus
@@ -26,19 +27,20 @@ logging.basicConfig(
     format='%(asctime)s %(name)s %(levelname)s %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S %p'
 )
-logger = logging.getLogger('acceptance-tool.tests.business_tests.test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO')
+logger = logging.getLogger('acceptance-tool.tests.business_tests.test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_RBAC_OK')
 logger.setLevel(logging.DEBUG)
 
 
 @pytest.mark.usefixtures('settings', scope='class')
-class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
+class Test_test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_ABAC_OK():
     """
-    Class to test the test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO use case:
+    #TODO:to update the comments
+    Class to test the test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_ABAC_OK use case:
     As a resource owner, i need the solution to prevent end users switching between applications in a timeframe smaller
-     than the allowed single sign on time span, to access applications they are not entitled to access.
+    than the allowed single sign on time span, to access applications they are not entitled to access.
     """
 
-    def test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO_SP_initiated(self, settings):
+    def test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_ABAC_OK_SP_initiated(self, settings):
         """
         Scenario: User logs in to SP1 where he has the appropriate attribute.
         Same user tries to log in to SP2, SP that he is not authorized to access. He should receive an
@@ -50,7 +52,7 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         s = Session()
 
         # Service provider settings
-        sp = settings["sps_saml"][0]
+        sp = settings["sps_wsfed"][0]
         sp_ip = sp["ip"]
         sp_port = sp["port"]
         sp_scheme = sp["http_scheme"]
@@ -58,12 +60,12 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         sp_message = sp["logged_in_message"]
 
         # Service provider 2 settings
-        sp2 = settings["sps_saml"][3]
+        sp2 = settings["sps_wsfed"][1]
         sp2_ip = sp2["ip"]
         sp2_port = sp2["port"]
         sp2_scheme = sp2["http_scheme"]
         sp2_path = sp2["path"]
-        sp2_message = settings["idp"]["not_authorized_message"]
+        sp2_message = sp2["logged_in_message"]
 
         # Identity provider settings
         idp_ip = settings["idp"]["ip"]
@@ -87,13 +89,9 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
 
         # Perform login to SP1
 
-        (session_cookie, response) = req.access_sp_saml(logger, s, header, sp_ip, sp_port, sp_scheme, sp_path,
-                                                        idp_ip, idp_port)
+        response = req.access_sp_ws_fed(logger, s, header, sp_ip, sp_port, sp_scheme, sp_path)
 
-        assert response.status_code == HTTPStatus.FOUND
-
-        # store the cookie received from keycloak
-        keycloak_cookie = response.cookies
+        session_cookie = response.cookies
 
         redirect_url = response.headers['Location']
 
@@ -103,7 +101,9 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
             'Referer': "{ip}:{port}".format(ip=sp_ip, port=sp_port)
         }
 
-        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp, keycloak_cookie)
+        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp, session_cookie)
+
+        keycloak_cookie = response.cookies
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -128,8 +128,11 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         credentials_data["username"] = idp_username
         credentials_data["password"] = idp_password
 
-        response = req.send_credentials_to_idp(logger, s, header, idp_ip, idp_port, redirect_url, url_form, credentials_data,
+        response = req.send_credentials_to_idp(logger, s, header, idp_ip, idp_port, redirect_url, url_form,
+                                               credentials_data,
                                                keycloak_cookie, method_form)
+
+        # print(response.text)
 
         assert response.status_code == HTTPStatus.OK or response.status_code == HTTPStatus.FOUND  #or response.status_code == 303 or response.status_code == 307
 
@@ -143,12 +146,13 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         method_form = form.get('method')
 
         # Get the SAML response from the identity provider
-        saml_response = {}
+        ws_fed_response = {}
         for input in inputs:
-            saml_response[input.get('name')] = input.get('value')
+            ws_fed_response[input.get('name')] = input.get('value')
 
-        (response, sp_cookie) = req.access_sp_with_token(logger, s, header, sp_ip, sp_port, idp_scheme, idp_ip, idp_port,
-                                                         method_form, url_form, saml_response, session_cookie,
+        (response, sp_cookie) = req.access_sp_with_token(logger, s, header, sp_ip, sp_port, idp_scheme, idp_ip,
+                                                         idp_port,
+                                                         method_form, url_form, ws_fed_response, session_cookie,
                                                          keycloak_cookie_2)
 
         assert response.status_code == HTTPStatus.OK
@@ -160,10 +164,9 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
 
         # Attempt to perform login on SP2
 
-        (session_cookie, response) = req.access_sp_saml(logger, s, header, sp2_ip, sp2_port, sp2_scheme, sp2_path, idp_ip,
-                                                        idp_port)
+        response = req.access_sp_ws_fed(logger, s, header, sp2_ip, sp2_port, sp2_scheme, sp2_path)
 
-        session_cookie2 = response.cookies
+        session_cookie = response.cookies
 
         redirect_url = response.headers['Location']
 
@@ -173,15 +176,29 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
             'Referer': "{ip}:{port}".format(ip=sp2_ip, port=sp2_port)
         }
 
-        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp, {**session_cookie2, **keycloak_cookie_2})
+        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp, {**keycloak_cookie_2})
 
-        # Assert that the client is not authorized to access SP2
-        assert response.status_code == HTTPStatus.FORBIDDEN
+        soup = BeautifulSoup(response.content, 'html.parser')
+        form = soup.body.form
+
+        url_form = form.get('action')
+        inputs = form.find_all('input')
+        method_form = form.get('method')
+
+        ws_fed_response = {}
+        for input in inputs:
+            ws_fed_response[input.get('name')] = input.get('value')
+
+        (response, sp2_cookie) = req.access_sp_with_token(logger, s, header, sp2_ip, sp2_port, idp_scheme, idp_ip,
+                                                          idp_port,
+                                                          method_form, url_form, ws_fed_response, session_cookie,
+                                                          keycloak_cookie_2)
+
+        assert response.status_code == HTTPStatus.OK
 
         assert re.search(sp2_message, response.text) is not None
 
-
-    def test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO_IDP_initiated(self, settings):
+    def test_CT_TC_WS_FED_IDP_ACCESS_CONTROL_ABAC_OK_IDP_initiated(self, settings):
         """
         Scenario: User logs in to the IDP. He then accesses SP1 where he has the appropriate attribute.
         Same user tries to log in to SP2, that he is not authorized to access. He should receive an
@@ -193,7 +210,7 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         s = Session()
 
         # Service provider settings
-        sp = settings["sps_saml"][0]
+        sp = settings["sps_wsfed"][0]
         sp_ip = sp["ip"]
         sp_port = sp["port"]
         sp_scheme = sp["http_scheme"]
@@ -201,12 +218,12 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         sp_message = sp["logged_in_message"]
 
         # Service provider 2 settings
-        sp2 = settings["sps_saml"][3]
+        sp2 = settings["sps_wsfed"][1]
         sp2_ip = sp2["ip"]
         sp2_port = sp2["port"]
         sp2_scheme = sp2["http_scheme"]
         sp2_path = sp2["path"]
-        sp2_message = settings["idp"]["not_authorized_message"]
+        sp2_message = sp2["logged_in_message"]
 
         # Identity provider settings
         idp_ip = settings["idp"]["ip"]
@@ -230,22 +247,19 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         }
 
         # Perform login to IDP
-        (oath_cookie, keycloak_cookie, keycloak_cookie2, response) = req.login_idp(logger, s, header, idp_ip,
-                                                                                   idp_port, idp_scheme,
-                                                                                   idp_path, idp_username,
+        (oath_cookie, keycloak_cookie, keycloak_cookie2, response) = req.login_idp(logger, s, header, idp_ip, idp_port,
+                                                                                   idp_scheme, idp_path, idp_username,
                                                                                    idp_password)
 
         assert response.status_code == HTTPStatus.OK
 
-        # Assert we are logged in to IDP
+        # Assert we are logged in
         assert re.search(idp_message, response.text) is not None
 
-        # Access SP1
-        (session_cookie, response) = req.access_sp_saml(logger, s, header, sp_ip, sp_port, sp_scheme, sp_path,
-                                                        idp_ip, idp_port)
+        response = req.access_sp_ws_fed(logger, s, header, sp_ip, sp_port, sp_scheme, sp_path)
 
         # store the cookie received from keycloak
-        keycloak_cookie3 = response.cookies
+        session_cookie = response.cookies
 
         assert response.status_code == HTTPStatus.FOUND
 
@@ -257,8 +271,7 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
             'Referer': "{ip}:{port}".format(ip=sp_ip, port=sp_port)
         }
 
-        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp,
-                                       {**keycloak_cookie3, **keycloak_cookie2})
+        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp, {**keycloak_cookie2})
 
         assert response.status_code == HTTPStatus.OK
 
@@ -270,13 +283,13 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
         method_form = form.get('method')
 
         # Get the saml response from the identity provider
-        saml_response = {}
+        ws_fed_response = {}
         for input in inputs:
-            saml_response[input.get('name')] = input.get('value')
+            ws_fed_response[input.get('name')] = input.get('value')
 
         (response, sp_cookie) = req.access_sp_with_token(logger, s, header, sp_ip, sp_port, idp_scheme, idp_ip,
                                                          idp_port,
-                                                         method_form, url_form, saml_response, session_cookie,
+                                                         method_form, url_form, ws_fed_response, session_cookie,
                                                          keycloak_cookie2)
 
         assert response.status_code == HTTPStatus.OK
@@ -287,11 +300,9 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
 
         # Attempt to access SP2
 
-        (session_cookie, response) = req.access_sp_saml(logger, s, header, sp2_ip, sp2_port, sp2_scheme, sp2_path,
-                                                        idp_ip,
-                                                        idp_port)
+        response = req.access_sp_ws_fed(logger, s, header, sp2_ip, sp2_port, sp2_scheme, sp2_path)
 
-        session_cookie2 = response.cookies
+        session_cookie = response.cookies
 
         redirect_url = response.headers['Location']
 
@@ -301,11 +312,25 @@ class Test_test_CT_TC_SAML_IDP_ACCESS_CONTROL_ABAC_KO():
             'Referer': "{ip}:{port}".format(ip=sp2_ip, port=sp2_port)
         }
 
-        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp,
-                                       {**session_cookie2, **keycloak_cookie2})
+        response = req.redirect_to_idp(logger, s, redirect_url, header_redirect_idp, {**keycloak_cookie2})
 
-        # Assert that the client is not authorized to access SP2
-        assert response.status_code == HTTPStatus.FORBIDDEN
+        soup = BeautifulSoup(response.content, 'html.parser')
+        form = soup.body.form
+
+        url_form = form.get('action')
+        inputs = form.find_all('input')
+        method_form = form.get('method')
+
+        ws_fed_response = {}
+        for input in inputs:
+            ws_fed_response[input.get('name')] = input.get('value')
+
+        (response, sp2_cookie) = req.access_sp_with_token(logger, s, header, sp2_ip, sp2_port, idp_scheme, idp_ip,
+                                                          idp_port,
+                                                          method_form, url_form, ws_fed_response, session_cookie,
+                                                          keycloak_cookie)
+
+        assert response.status_code == HTTPStatus.OK
 
         assert re.search(sp2_message, response.text) is not None
 
